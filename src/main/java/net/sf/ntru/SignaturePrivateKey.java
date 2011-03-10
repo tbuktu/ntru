12b7aeca8b7a86bb68aba5bae140696caad5f2ff
T. Buktu <invalid@nowhere.com>
2011-03-09 17:10:57 -0700

    Change license, add license headers

--- src/main/java/net/sf/ntru/SignaturePrivateKey.java
+++ src/main/java/net/sf/ntru/SignaturePrivateKey.java
@@ -1,3 +1,21 @@
+/**
+ * This software is dual-licensed. You may choose either the
+ * Non-Profit Open Software License version 3.0, or any license
+ * agreement into which you enter with Security Innovation, Inc.
+ * 
+ * Use of this code, or certain portions thereof, implements
+ * inventions covered by claims of one or more of the following
+ * U.S. Patents and/or foreign counterpart patents, owned by
+ * Security Innovation, Inc.:
+ * 7,308,097, 7,031,468, 6,959,085, 6,298,137, and 6,081,597.
+ * Practice or sale of the inventions embodied in the code hereof
+ * requires a license from Security Innovation Inc. at:
+ * 
+ * 187 Ballardvale St, Suite A195
+ * Wilmington, MA 01887
+ * USA
+ */
+
 package net.sf.ntru;
 
 import java.io.ByteArrayOutputStream;
