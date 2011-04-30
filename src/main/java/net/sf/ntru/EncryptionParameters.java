6d93f9562e846d7b7523f9c40c08facb383fb308
T. Buktu <invalid@nowhere.com>
2011-04-29 22:16:40 -0700

    Fix endless loops in IGF caused by values of c < log2(N)

--- src/main/java/net/sf/ntru/EncryptionParameters.java
+++ src/main/java/net/sf/ntru/EncryptionParameters.java
@@ -20,10 +20,10 @@ package net.sf.ntru;
 
 public class EncryptionParameters {
     public static final EncryptionParameters EES1087EP2 = new EncryptionParameters(1087, 2048, 120, 120, 256, 13, 25, 14, new byte[] {0, 6, 3}, true);   // security>256, optimized for key size
-    public static final EncryptionParameters EES1171EP1 = new EncryptionParameters(1171, 2048, 106, 106, 256, 10, 20, 15, new byte[] {0, 6, 4}, true);   // security>256, key size / speed tradeoff
+    public static final EncryptionParameters EES1171EP1 = new EncryptionParameters(1171, 2048, 106, 106, 256, 13, 20, 15, new byte[] {0, 6, 4}, true);   // security>256, key size / speed tradeoff
     public static final EncryptionParameters EES1499EP1 = new EncryptionParameters(1499, 2048, 79, 79, 256, 13, 17, 19, new byte[] {0, 6, 5}, true);   // security>256, optimized for speed
     public static final EncryptionParameters APR2011_439 = new EncryptionParameters(439, 2048, 146, 130, 128, 9, 32, 9, new byte[] {0, 7, 101}, true);   // security=128
-    public static final EncryptionParameters APR2011_743 = new EncryptionParameters(743, 2048, 248, 220, 256, 9, 27, 14, new byte[] {0, 7, 105}, false);   // security=256
+    public static final EncryptionParameters APR2011_743 = new EncryptionParameters(743, 2048, 248, 220, 256, 10, 27, 14, new byte[] {0, 7, 105}, false);   // security=256
     
     int N, q, df, dr, dg, llen, maxMsgLenBytes, db, bufferLenBits, bufferLenTrits, dm0, pkLen, c, minCallsR, minCallsMask;
     byte[] oid;
