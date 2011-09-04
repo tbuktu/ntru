0ce4fbfb2bf7f1b0b541af13baaef701b444ca3c
T. Buktu <tbuktu@hotmail.com>
2011-09-04 00:44:13 -0700

    Make sure code is compiled

--- bench.sh
+++ bench.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 MAVEN=mvn
+${MAVEN} compile
 command -v ${MAVEN} >/dev/null || { echo "Error: mvn not found or Maven not installed." >&2; exit 1; }
 ${MAVEN} exec:java -Dexec.mainClass="net.sf.ntru.demo.Benchmark" -Dexec.args="$@"
