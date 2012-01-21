tbuktu/ntru

commit 7cb6e682fb4f2a69e14ce58bf99c2a6ad4ffa611

More refactoring

src/main/java/net/sf/ntru/polynomial/IntegerPolynomial.java
@@ -371,7 +371,7 @@ public class IntegerPolynomial implements Polynomial {
      * The algorithm is described in <a href="http://www.securityinnovation.com/uploads/Crypto/NTRUTech014.pdf">
      * Almost Inverses and Fast NTRU Key Generation</a>.
      * @param q the modulus
-     * @return a new polynomial
+     * @return a new polynomial, or <code>null</code> if no inverse exists
      */
     public IntegerPolynomial invertFq(int q) {
         int N = coeffs.length;
@@ -379,9 +379,8 @@ public class IntegerPolynomial implements Polynomial {
         IntegerPolynomial b = new IntegerPolynomial(N+1);
         b.coeffs[0] = 1;
         IntegerPolynomial c = new IntegerPolynomial(N+1);
-        IntegerPolynomial f = new IntegerPolynomial(N+1);
-        f.coeffs = Arrays.copyOf(coeffs, N+1);
-        f.modPositive(2);
+        IntegerPolynomial f = new IntegerPolynomial(Arrays.copyOf(coeffs, N+1));
+        f.mod(2);
         // set g(x) = x^N − 1
         IntegerPolynomial g = new IntegerPolynomial(N+1);
         g.coeffs[0] = 1;
@@ -472,7 +471,7 @@ public class IntegerPolynomial implements Polynomial {
      * Returns <code>null</code> if the polynomial is not invertible.<br/>
      * The algorithm is described in <a href="http://www.securityinnovation.com/uploads/Crypto/NTRUTech014.pdf">
      * Almost Inverses and Fast NTRU Key Generation</a>.
-     * @return a new polynomial
+     * @return a new polynomial, or <code>null</code> if no inverse exists
      */
     public IntegerPolynomial invertF3() {
         int N = coeffs.length;
@@ -482,7 +481,7 @@ public class IntegerPolynomial implements Polynomial {
         IntegerPolynomial c = new IntegerPolynomial(N+1);
         IntegerPolynomial f = new IntegerPolynomial(N+1);
         f.coeffs = Arrays.copyOf(coeffs, N+1);
-        f.modPositive(3);
+        f.mod(3);
         // set g(x) = x^N − 1
         IntegerPolynomial g = new IntegerPolynomial(N+1);
         g.coeffs[0] = -1;
@@ -738,6 +737,13 @@ public class IntegerPolynomial implements Polynomial {
             coeffs = Arrays.copyOf(coeffs, b.coeffs.length);
         for (int i=0; i<b.coeffs.length; i++)
             coeffs[i] += b.coeffs[i];
+    }
+    
+    public void add(TernaryPolynomial b) {
+        for (int i: b.getOnes())
+            coeffs[i]++;
+        for (int i: b.getNegOnes())
+            coeffs[i]--;
     }
     
     /**

src/main/java/net/sf/ntru/encrypt/NtruEncrypt.java
@@ -154,13 +154,17 @@ public class NtruEncrypt {
             // choose random t, calculate f and fp
             if (fastFp) {
                 // if fastFp=true, f is always invertible mod 3
-                t = params.polyType==TernaryPolynomialType.SIMPLE ? PolynomialGenerator.generateRandomTernary(N, df, df, sparse, rngf) : ProductFormPolynomial.generateRandom(N, df1, df2, df3, df3, rngf);
+                t = params.polyType==TernaryPolynomialType.SIMPLE ?
+                        PolynomialGenerator.generateRandomTernary(N, df, df, sparse, rngf) :
+                        ProductFormPolynomial.generateRandom(N, df1, df2, df3, df3, rngf);
                 f = t.toIntegerPolynomial();
                 f.mult(3);
                 f.coeffs[0] += 1;
             }
             else {
-                t = params.polyType==TernaryPolynomialType.SIMPLE ? PolynomialGenerator.generateRandomTernary(N, df, df-1, sparse, rngf) : ProductFormPolynomial.generateRandom(N, df1, df2, df3, df3-1, rngf);
+                t = params.polyType==TernaryPolynomialType.SIMPLE ?
+                        PolynomialGenerator.generateRandomTernary(N, df, df-1, sparse, rngf) :
+                        ProductFormPolynomial.generateRandom(N, df1, df2, df3, df3-1, rngf);
                 f = t.toIntegerPolynomial();
                 fp = f.invertF3();
                 if (fp == null)
@@ -168,9 +172,8 @@ public class NtruEncrypt {
             }
             
             fq = f.invertFq(q);
-            if (fq == null)
-                continue;
-            break;
+            if (fq != null)
+                break;
         }
         
         // if fastFp=true, fp=1
@@ -494,15 +497,17 @@ public class NtruEncrypt {
      * @return
      */
     IntegerPolynomial decrypt(IntegerPolynomial e, Polynomial priv_t, IntegerPolynomial priv_fp) {
+        int q = params.q;
+        
         IntegerPolynomial a;
         if (params.fastFp) {
-            a = priv_t.mult(e, params.q);
+            a = priv_t.mult(e, q);
             a.mult(3);
             a.add(e);
         }
         else
-            a = priv_t.mult(e, params.q);
-        a.center0(params.q);
+            a = priv_t.mult(e, q);
+        a.center0(q);
         a.mod3();
         
         IntegerPolynomial c = params.fastFp ? a : new DenseTernaryPolynomial(a).mult(priv_fp, 3);

src/main/java/net/sf/ntru/polynomial/DenseTernaryPolynomial.java
@@ -22,8 +22,6 @@ import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Random;
 
-import net.sf.ntru.exception.NtruException;
-
 /**
  * A <code>TernaryPolynomial</code> with a "high" number of nonzero coefficients.
  */
@@ -35,7 +33,6 @@ public class DenseTernaryPolynomial extends IntegerPolynomial implements Ternary
      */
     DenseTernaryPolynomial(int N) {
         super(N);
-        checkTernarity();
     }
     
     /**
@@ -53,13 +50,6 @@ public class DenseTernaryPolynomial extends IntegerPolynomial implements Ternary
      */
     public DenseTernaryPolynomial(int[] coeffs) {
         super(coeffs);
-        checkTernarity();
-    }
-    
-    private void checkTernarity() {
-        for (int c: coeffs)
-            if (c<-1 || c>1)
-                throw new NtruException("Illegal value: " + c + ", must be one of {-1, 0, 1}");
     }
     
     /**

src/main/java/net/sf/ntru/polynomial/ProductFormPolynomial.java
@@ -99,7 +99,7 @@ public class ProductFormPolynomial implements Polynomial {
     @Override
     public IntegerPolynomial toIntegerPolynomial() {
         IntegerPolynomial i = f1.mult(f2.toIntegerPolynomial());
-        i.add(f3.toIntegerPolynomial());
+        i.add(f3);
         return i;
     }
 

src/main/java/net/sf/ntru/sign/NtruSign.java
@@ -380,7 +380,9 @@ public class NtruSign {
         Random rng = new SecureRandom();
         do {
             do {
-                f = params.polyType==TernaryPolynomialType.SIMPLE ? DenseTernaryPolynomial.generateRandom(N, d+1, d, rng) : ProductFormPolynomial.generateRandom(N, d1, d2, d3+1, d3, rng);
+                f = params.polyType==TernaryPolynomialType.SIMPLE ?
+                        DenseTernaryPolynomial.generateRandom(N, d+1, d, rng) :
+                        ProductFormPolynomial.generateRandom(N, d1, d2, d3+1, d3, rng);
                 fInt = f.toIntegerPolynomial();
             } while (primeCheck && fInt.resultant(_2n1).res.equals(ZERO));
             fq = fInt.invertFq(q);
@@ -390,7 +392,9 @@ public class NtruSign {
         do {
             do {
                 do {
-                    g = params.polyType==TernaryPolynomialType.SIMPLE ? DenseTernaryPolynomial.generateRandom(N, d+1, d, rng) : ProductFormPolynomial.generateRandom(N, d1, d2, d3+1, d3, rng);
+                    g = params.polyType==TernaryPolynomialType.SIMPLE ?
+                            DenseTernaryPolynomial.generateRandom(N, d+1, d, rng) :
+                            ProductFormPolynomial.generateRandom(N, d1, d2, d3+1, d3, rng);
                     gInt = g.toIntegerPolynomial();
                 } while (primeCheck && gInt.resultant(_2n1).res.equals(ZERO));
             } while (gInt.invertFq(q) == null);

src/test/java/net/sf/ntru/polynomial/DenseTernaryPolynomialTest.java
@@ -1,0 +1,28 @@
+package net.sf.ntru.polynomial;
+
+import static org.junit.Assert.assertTrue;
+
+import java.util.Random;
+
+import org.junit.Test;
+
+public class DenseTernaryPolynomialTest {
+    
+    @Test
+    public void testGenerateRandom() {
+        checkTernarity(DenseTernaryPolynomial.generateRandom(1499));
+        
+        Random rng = new Random();
+        for (int i=0; i<10; i++) {
+            int N = rng.nextInt(2000) + 10;
+            int numOnes = rng.nextInt(N);
+            int numNegOnes = rng.nextInt(N-numOnes);
+            checkTernarity(DenseTernaryPolynomial.generateRandom(N, numOnes, numNegOnes, rng));
+        }
+    }
+    
+    private void checkTernarity(DenseTernaryPolynomial poly) {
+        for (int c: poly.coeffs)
+            assertTrue(c>=-1 && c<=1);
+    }
+}

src/test/java/net/sf/ntru/polynomial/IntegerPolynomialTest.java
@@ -27,6 +27,7 @@ import java.math.BigInteger;
 import java.security.SecureRandom;
 import java.util.Arrays;
 
+import net.sf.ntru.encrypt.EncryptionParameters;
 import net.sf.ntru.sign.SignatureParameters;
 
 import org.junit.Test;
@@ -211,5 +212,20 @@ public class IntegerPolynomialTest {
         else
             assertEquals(BigInteger.ZERO, (b.coeffs[0].subtract(b.coeffs[b.coeffs.length-1]).subtract(r.res).mod(BigInteger.valueOf(p))));
         assertEquals(BigInteger.ZERO, b.coeffs[0].subtract(r.res).subtract(b.coeffs[b.coeffs.length-1].negate()).mod(BigInteger.valueOf(p)));
+    }
+    
+    @Test
+    public void testAdd() {
+        EncryptionParameters params = EncryptionParameters.EES1087EP2;
+        IntegerPolynomial a = PolynomialGeneratorForTesting.generateRandom(params.N, params.q);
+        TernaryPolynomial b = DenseTernaryPolynomial.generateRandom(1087);
+        
+        IntegerPolynomial c1 = a.clone();
+        c1.add(b.toIntegerPolynomial());
+        
+        IntegerPolynomial c2 = a.clone();
+        c2.add(b);
+        
+        assertEquals(c1, c2);
     }
 }

commit 78542be78ab1d378ef7029dfdc5f48e99bcabe08

Fix performance regression introduced in commit 7cb6e682fb4f2a69e14ce58bf99c2a6ad4ffa611 that slowed down NtruEncrypt key generation

src/main/java/net/sf/ntru/polynomial/IntegerPolynomial.java
@@ -382,7 +382,7 @@ public class IntegerPolynomial implements Polynomial {
         b.coeffs[0] = 1;
         IntegerPolynomial c = new IntegerPolynomial(N+1);
         IntegerPolynomial f = new IntegerPolynomial(Arrays.copyOf(coeffs, N+1));
-        f.mod(2);
+        f.modPositive(2);
         // set g(x) = x^N − 1
         IntegerPolynomial g = new IntegerPolynomial(N+1);
         g.coeffs[0] = 1;
@@ -483,7 +483,7 @@ public class IntegerPolynomial implements Polynomial {
         IntegerPolynomial c = new IntegerPolynomial(N+1);
         IntegerPolynomial f = new IntegerPolynomial(N+1);
         f.coeffs = Arrays.copyOf(coeffs, N+1);
-        f.mod(3);
+        f.modPositive(3);
         // set g(x) = x^N − 1
         IntegerPolynomial g = new IntegerPolynomial(N+1);
         g.coeffs[0] = -1;