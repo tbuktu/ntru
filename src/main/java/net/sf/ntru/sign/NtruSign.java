tbuktu/ntru

commit a74e4d586c05ab7cd176e31db66fd22920bd382c

Don't use more threads than processors are available

src/main/java/net/sf/ntru/sign/NtruSign.java
@@ -67,28 +67,44 @@ public class NtruSign {
     }
     
     /**
-     * Generates a new signature key pair. Starts <code>B+1</code> threads.
+     * Generates a new signature key pair. Uses up to <code>B+1</code> threads
+     * if multiple processors are available.
      * @return a key pair
      */
     public SignatureKeyPair generateKeyPair() {
+        int processors = Runtime.getRuntime().availableProcessors();
         SignaturePrivateKey priv = new SignaturePrivateKey();
-        SignaturePublicKey pub = null;
-        ExecutorService executor = Executors.newCachedThreadPool();
-        List<Future<Basis>> bases = new ArrayList<Future<Basis>>();
-        for (int k=params.B; k>=0; k--)
-            bases.add(executor.submit(new BasisGenerationTask()));
-        executor.shutdown();
         
-        for (int k=params.B; k>=0; k--) {
-            Future<Basis> basis = bases.get(k);
-            try {
-                priv.add(basis.get());
-            if (k == 0)
-                pub = new SignaturePublicKey(basis.get().h, params);
-            } catch (Exception e) {
-                throw new NtruException(e);
+        if (processors == 1)
+            // generate all B+1 bases in the current thread
+            for (int k=params.B; k>=0; k--)
+                priv.add(generateBoundedBasis());
+        else {
+            List<Future<Basis>> bases = new ArrayList<Future<Basis>>();
+            
+            // start up to processors-1 new threads and generate B bases
+            int numThreads = Math.min(params.B, processors-1);
+            if (numThreads > 0) {
+                ExecutorService executor = Executors.newFixedThreadPool(numThreads);
+                for (int k=params.B-1; k>=0; k--)
+                    bases.add(executor.submit(new BasisGenerationTask()));
+                executor.shutdown();
             }
+            
+            // generate the remaining basis in the current thread
+            Basis basis0 = generateBoundedBasis();
+            
+            // build the private key
+            priv.add(basis0);
+            for (Future<Basis> basis: bases)
+                try {
+                    priv.add(basis.get());
+                } catch (Exception e) {
+                    throw new NtruException(e);
+                }
         }
+        
+        SignaturePublicKey pub = new SignaturePublicKey(priv.getBasis(0).h, params);
         SignatureKeyPair kp = new SignatureKeyPair(priv, pub);
         return kp;
     }

commit 34a79f0ca15769d838e68534fc362d5f040c699d

Fix a bug introduced in commit a74e4d586c05ab7cd176e31db66fd22920bd382c that caused invalid signatures for APR2011_743* params

src/main/java/net/sf/ntru/sign/NtruSign.java
@@ -95,16 +95,16 @@ public class NtruSign {
             Basis basis0 = generateBoundedBasis();
             
             // build the private key
-            priv.add(basis0);
             for (Future<Basis> basis: bases)
                 try {
                     priv.add(basis.get());
                 } catch (Exception e) {
                     throw new NtruException(e);
                 }
+            priv.add(basis0);
         }
         
-        SignaturePublicKey pub = new SignaturePublicKey(priv.getBasis(0).h, params);
+        SignaturePublicKey pub = new SignaturePublicKey(priv.getBasis(params.B).h, params);
         SignatureKeyPair kp = new SignatureKeyPair(priv, pub);
         return kp;
     }