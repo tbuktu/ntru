e98947c5e7c0ed38b7dcefcfdb3e3f2208d2f07f
T. Buktu <tbuktu@hotmail.com>
2012-02-18 16:03:56 -0700

    Do each step in its own AsyncTask

--- src/main/android/net/sf/ntru/demo/NtruActivity.java
+++ src/main/android/net/sf/ntru/demo/NtruActivity.java
@@ -1,7 +1,5 @@
 package net.sf.ntru.demo;
 
-import java.text.DecimalFormat;
-
 import net.sf.ntru.encrypt.EncryptionKeyPair;
 import net.sf.ntru.encrypt.EncryptionParameters;
 import net.sf.ntru.encrypt.NtruEncrypt;
@@ -10,6 +8,7 @@ import net.sf.ntru.sign.SignatureKeyPair;
 import net.sf.ntru.sign.SignatureParameters;
 import android.app.Activity;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.SystemClock;
@@ -17,8 +16,18 @@ import android.util.Base64;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+/**
+ * A simple non-interactive app that encrypts, decrypts, signs a test message and
+ * verifies the signature. It prints the results on the screen including the time
+ * it took for each operation.
+ */
 public class NtruActivity extends Activity {
     private LinearLayout layout;
+    NtruEncrypt ntruEnc;
+    NtruSign ntruSig;
+    EncryptionKeyPair encKP;
+    SignatureKeyPair sigKP;
+    String msg = "The quick brown fox";
     
     /** Called when the activity is first created. */
     @Override
@@ -29,22 +38,18 @@ public class NtruActivity extends Activity {
         layout.setOrientation(LinearLayout.VERTICAL);
         setContentView(layout);
         
-        new EncryptTask().execute();
+        ntruEnc = new NtruEncrypt(EncryptionParameters.APR2011_439_FAST);
+        ntruSig = new NtruSign(SignatureParameters.TEST157_PROD);
+        new EncKeyGenTask().execute();
     }
     
-    /**
-     * Generates an encryption key pair, encrypts a message, and decrypts it.
-     * Prints text on the screen for each step.
-     */
-    private class EncryptTask extends AsyncTask<Void, Void, EncryptionKeyPair> {
-        NtruEncrypt ntru;
+    /** Generates an encryption key pair and calls EncryptTask when finished */
+    private class EncKeyGenTask extends AsyncTask<Void, Void, Void> {
+        long duration;
         TextView view;
-        long startTime;   // the time key generation started
-        
-        EncryptTask() {
+
+        EncKeyGenTask() {
             println("NTRU encryption", Color.WHITE);
-            
-            ntru = new NtruEncrypt(EncryptionParameters.APR2011_439_FAST);
             
             view = new TextView(NtruActivity.this);
             view.setTextColor(Color.GREEN);
@@ -53,79 +58,169 @@ public class NtruActivity extends Activity {
         }
         
         @Override
-        protected EncryptionKeyPair doInBackground(Void... params) {
-            startTime = SystemClock.uptimeMillis();
-            return ntru.generateKeyPair();
+        protected Void doInBackground(Void... arg0) {
+            long startTime = SystemClock.uptimeMillis();
+            encKP = ntruEnc.generateKeyPair();
+            long endTime = SystemClock.uptimeMillis();
+            duration = endTime - startTime;
+            return null;
         }
         
         @Override
-        protected void onPostExecute(EncryptionKeyPair kp) {
-            long endTime = SystemClock.uptimeMillis();
-            String duration = DecimalFormat.getNumberInstance().format((endTime-startTime)/1000.0);
-            view.append(" done (" + duration + "s)");
-            encryptDecrypt(ntru, kp);
-        }
-        
-        private void encryptDecrypt(NtruEncrypt ntru, EncryptionKeyPair kp) {
-            String msg = "The quick brown fox";
-            println("  Before encryption: ", Color.GREEN, msg, Color.GRAY);
-            
-            byte[] enc = ntru.encrypt(msg.getBytes(), kp.getPublic());
-            String encStr = Base64.encodeToString(enc, Base64.DEFAULT).substring(0, 20) + "...";
-            println("  After encryption: ", Color.GREEN, encStr, Color.GRAY);
-            
-            byte[] dec = ntru.decrypt(enc, kp);
-            println("  After decryption: ", Color.GREEN, new String(dec), Color.GRAY);
-            
-            println("", Color.WHITE);
-            new SignTask().execute();
+        protected void onPostExecute(Void result) {
+            view.append(" done (" + duration + "ms)");
+            new EncryptTask().execute();
         }
     }
     
-    /**
-     * Generates a signature key pair, signs a message, and verifies the signature.
-     * Prints text on the screen for each step.
-     */
-    private class SignTask extends AsyncTask<Void, Void, SignatureKeyPair> {
-        NtruSign ntru;
+    /** Encrypts a message and calls DecryptTask */
+    private class EncryptTask extends AsyncTask<Void, Void, byte[]> {
         TextView view;
-        long startTime;   // the time key generation started
+        long startTime;
         
-        SignTask() {
-            println("NTRU signature", Color.WHITE);
+        EncryptTask() {
+            println("  Before encryption: ", Color.GREEN, msg, Color.GRAY);
             
-            ntru = new NtruSign(SignatureParameters.TEST157_PROD);
-            
-            println("  Generating key pair", Color.GREEN);
             view = new TextView(NtruActivity.this);
             view.setTextColor(Color.GREEN);
-            view.setText("  (may take several minutes)...");
+            view.setText("  Encrypting...");
             layout.addView(view);
         }
         
         @Override
-        protected SignatureKeyPair doInBackground(Void... params) {
+        protected byte[] doInBackground(Void... params) {
             startTime = SystemClock.uptimeMillis();
-            return ntru.generateKeyPair();
+            return ntruEnc.encrypt(msg.getBytes(), encKP.getPublic());
         }
         
         @Override
-        protected void onPostExecute(SignatureKeyPair kp) {
+        protected void onPostExecute(byte[] encrypted) {
             long endTime = SystemClock.uptimeMillis();
-            String duration = DecimalFormat.getNumberInstance().format((endTime-startTime)/1000.0);
-            view.append(" done (" + duration + "s)");
-            signVerify(ntru, kp);
+            view.append(" done (" + (endTime-startTime) + "ms)");
+            new DecryptTask(encrypted).execute();
+        }
+    }
+    
+    /** Decrypts a message and calls SigKeyGenTask */
+    private class DecryptTask extends AsyncTask<Void, Void, byte[]> {
+        TextView view;
+        long startTime;
+        byte[] encrypted;
+        
+        DecryptTask(byte[] encrypted) {
+            this.encrypted = encrypted;
+            
+            String encStr = Base64.encodeToString(encrypted, Base64.DEFAULT).substring(0, 20) + "...";
+            println("  After encryption: ", Color.GREEN, encStr, Color.GRAY);
+            
+            view = new TextView(NtruActivity.this);
+            view.setTextColor(Color.GREEN);
+            view.setText("  Decrypting...");
+            layout.addView(view);
         }
         
-        private void signVerify(NtruSign ntru, SignatureKeyPair kp) {
-            String msg = "The quick brown fox";
+        @Override
+        protected byte[] doInBackground(Void... params) {
+            startTime = SystemClock.uptimeMillis();
+            return ntruEnc.decrypt(encrypted, encKP);
+        }
+        
+        @Override
+        protected void onPostExecute(byte[] decrypted) {
+            long endTime = SystemClock.uptimeMillis();
+            view.append(" done (" + (endTime-startTime) + "ms)");
+            println("  After decryption: ", Color.GREEN, new String(decrypted), Color.GRAY);
+            println("", Color.WHITE);
+            new SigKeyGenTask().execute();
+        }
+    }
+        
+    /** Generates an signing key pair and calls SignTask when finished */
+    private class SigKeyGenTask extends AsyncTask<Void, Void, Void> {
+        long duration;
+        TextView view;
+
+        SigKeyGenTask() {
+            println("NTRU signature", Color.WHITE);
+            
+            view = new TextView(NtruActivity.this);
+            view.setTextColor(Color.GREEN);
+            view.setText("  Generating key pair...");
+            layout.addView(view);
+        }
+        
+        @Override
+        protected Void doInBackground(Void... arg0) {
+            long startTime = SystemClock.uptimeMillis();
+            sigKP = ntruSig.generateKeyPair();
+            long endTime = SystemClock.uptimeMillis();
+            duration = endTime - startTime;
+            return null;
+        }
+        
+        @Override
+        protected void onPostExecute(Void result) {
+            view.append(" done (" + duration/1000 + "s)");
+            new SignTask().execute();
+        }
+    }
+    
+    /** Signs a message and calls VerifyTask */
+    private class SignTask extends AsyncTask<Void, Void, byte[]> {
+        TextView view;
+        long startTime;
+        
+        SignTask() {
             println("  Message: ", Color.GREEN, msg, Color.GRAY);
             
-            byte[] sig = ntru.sign(msg.getBytes(), kp);
-            String sigStr = Base64.encodeToString(sig, Base64.DEFAULT).substring(0, 20) + "...";
+            view = new TextView(NtruActivity.this);
+            view.setTextColor(Color.GREEN);
+            view.setText("  Signing...");
+            layout.addView(view);
+        }
+        
+        @Override
+        protected byte[] doInBackground(Void... params) {
+            startTime = SystemClock.uptimeMillis();
+            return ntruSig.sign(msg.getBytes(), sigKP);
+        }
+        
+        @Override
+        protected void onPostExecute(byte[] signature) {
+            long endTime = SystemClock.uptimeMillis();
+            view.append(" done (" + (endTime-startTime) + "ms)");
+            new VerifyTask(signature).execute();
+        }
+    }
+    
+    /** Verifies a signature */
+    private class VerifyTask extends AsyncTask<Void, Void, Boolean> {
+        TextView view;
+        long startTime;
+        byte[] signature;
+        
+        VerifyTask(byte[] signature) {
+            this.signature = signature;
+            
+            String sigStr = Base64.encodeToString(signature, Base64.DEFAULT).substring(0, 20) + "...";
             println("  Signature: ", Color.GREEN, sigStr, Color.GRAY);
             
-            boolean valid = ntru.verify(msg.getBytes(), sig, kp.getPublic());
+            view = new TextView(NtruActivity.this);
+            view.setTextColor(Color.GREEN);
+            view.setText("  Verifying...");
+            layout.addView(view);
+        }
+        
+        @Override
+        protected Boolean doInBackground(Void... params) {
+            startTime = SystemClock.uptimeMillis();
+            return ntruSig.verify(msg.getBytes(), signature, sigKP.getPublic());
+        }
+        
+        @Override
+        protected void onPostExecute(Boolean valid) {
+            long endTime = SystemClock.uptimeMillis();
+            view.append(" done (" + (endTime-startTime) + "ms)");
             println("  Signature valid? ", Color.GREEN, String.valueOf(valid), Color.GRAY);
 
             println("", Color.WHITE);
@@ -150,6 +245,7 @@ public class NtruActivity extends Activity {
         row.addView(view);
         
         view = new TextView(this);
+        view.setTypeface(Typeface.MONOSPACE);
         view.setTextColor(color2);
         view.setText(text2);
         row.addView(view);
